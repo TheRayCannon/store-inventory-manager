9b820ceead475576e20e7f539e29d22a8fc231b1
Shawn Cannon <scanno16@ford.com>
2022-04-26 10:42:48 -0600

    set catiogories

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
+const { it } = require("eslint/lib/rule-tester/rule-tester")
+
 const form = document.querySelector("form")
 const inventoryBox = document.querySelector(".inventoryBox")
 let stock = [{
     item: "+5 Dexterity Vest",
     sellBy: 10,
-    quality: 20
+    quality: 20,
+    catigory: none
 }, {
     item: "Aged Brie",
     sellBy: 2,
-    quality: 0
+    quality: 0,
+    catigory: "Aged Brie"
 }, {
     item: "Elixir of the Mongoose",
     sellBy: 5,
-    quality: 7
+    quality: 7,
+    catigory: "none"
 }, {
     item: "Sulfuras, Hand of Ragnaros",
     sellBy: 0,
-    quality: 80
+    quality: 80,
+    catigory: "Sulfuras"
 }, {
     item: "Backstage passes to a TAFKAL80ETC concert",
     sellBy: 15,
-    quality: 20
+    quality: 20,
+    catigory: "Backstage passes"
 }, {
     item: "Conjured Mana Cake",
     sellBy: 3,
-    quality: 6
+    quality: 6,
+    catigory: "Conjured"
 }]
-
-mapIt(stock);
-
-addEventListener("submit", event => {
-    event.preventDefault()
-    const formData = new FormData(event.target)
-    const newItem = {
-        item: formData.get("item"),
-        sellBy: formData.get("sell_in"),
-        quality: formData.get("quality")
-    }
-    stock = [...stock, newItem]
-    mapIt(stock)
-});
 
 
 function mapIt(stock) {
@@ -49,9 +43,40 @@ function mapIt(stock) {
  <h3>Item:${items.item}</h3>
  <p>Sell in ${items.sellBy} Days</p>
  <p>Quality:${items.quality}</p>
+ <p>Catigory:${items.catigory}</p>
 `
         return inventoryDisplay
     }).forEach((inventoryDisplay) => {
         inventoryBox.append(inventoryDisplay)
     })
 }
+
+
+mapIt(stock);
+
+function catigorySet(newItem) {
+    if (item.item.inlucdes("Conjured")) {
+        item.catigory = "conjured"
+    } else if (item.item.inlucdes("Aged Brie")) {
+        item.catigory = "Aged Brie"
+    } else if (item.item.inlucdes("Backstage passes")) {
+        item.catigory = "Backstage passes"
+    } else if (item.item.inlucdes("Sulfuras")) {
+        item.catigory = "Sulfuras"
+    } else {
+        item.catigory = none
+    }
+}
+
+addEventListener("submit", event => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const newItem = {
+        item: formData.get("item"),
+        sellBy: formData.get("sell_in"),
+        quality: formData.get("quality"),
+        // catigory: catigorySet(newItem)
+    }
+    stock = [...stock, newItem]
+    mapIt(stock)
+});
